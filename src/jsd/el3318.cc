d1c64218eb8f6c1f2e38efcba7803da0ebb3bc6f
David Kim <david.inkyu.kim@jpl.nasa.gov>
2022-02-28 09:50:18 -0800

    Added el3318 element config

--- src/jsd/el3318.cc
+++ src/jsd/el3318.cc
@@ -37,30 +37,20 @@ bool fastcat::El3318::ConfigFromYamlCommon(YAML::Node node)
   jsd_slave_config_.product_code         = JSD_EL3318_PRODUCT_CODE;
   snprintf(jsd_slave_config_.name, JSD_NAME_LEN, "%s", name_.c_str());
 
-  jsd_slave_config_.el3318.element[0] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[1] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[2] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[3] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[4] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[5] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[6] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.element[7] = JSD_EL3318_ELEMENT_TYPE_K;
-  jsd_slave_config_.el3318.filter[0] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[1] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[2] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[3] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[4] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[5] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[6] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.filter[7] = JSD_BECKHOFF_FILTER_50HZ;
-  jsd_slave_config_.el3318.presentation[0] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[1] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[2] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[3] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[4] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[5] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[6] = JSD_EL3318_PRESENTATION_SIGNED;
-  jsd_slave_config_.el3318.presentation[7] = JSD_EL3318_PRESENTATION_SIGNED;
+  if (!ParseVal(node, "element", element_string_)) {
+    return false;
+  }
+
+  if (!ElementFromString(element_string_, element_)) {
+    return false;
+  }
+
+  int i;
+  for(i=0; i < JSD_EL3318_NUM_CHANNELS; i++) {
+    jsd_slave_config_.el3318.element[i] = element_;
+    jsd_slave_config_.el3318.filter[i] = JSD_BECKHOFF_FILTER_50HZ;
+    jsd_slave_config_.el3318.presentation[i] = JSD_EL3318_PRESENTATION_SIGNED;
+  }
 
   return true;
 }
@@ -89,6 +79,40 @@ bool fastcat::El3318::Read()
   state_->el3318_state.adc_value_ch6 = jsd_state->adc_value[5];
   state_->el3318_state.adc_value_ch7 = jsd_state->adc_value[6];
   state_->el3318_state.adc_value_ch8 = jsd_state->adc_value[7];
+
+  return true;
+}
+
+bool fastcat::El3318::ElementFromString(std::string element_string,
+                                        jsd_el3318_element_t& element)
+{
+  MSG("Converting element to string.");
+  if (element_string.compare("TYPE_K") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_K;
+  } else if (element_string.compare("TYPE_J") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_J;
+  } else if (element_string.compare("TYPE_L") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_L;
+  } else if (element_string.compare("TYPE_E") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_E;
+  } else if (element_string.compare("TYPE_T") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_T;
+  } else if (element_string.compare("TYPE_N") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_N;
+  } else if (element_string.compare("TYPE_U") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_U;
+  } else if (element_string.compare("TYPE_B") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_B;
+  } else if (element_string.compare("TYPE_R") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_R;
+  } else if (element_string.compare("TYPE_S") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_S;
+  } else if (element_string.compare("TYPE_C") == 0) {
+    element = JSD_EL3318_ELEMENT_TYPE_C;
+  } else {
+    ERROR("El3318 element %s is invalid", element_string.c_str());
+    return false;
+  }
 
   return true;
 }
