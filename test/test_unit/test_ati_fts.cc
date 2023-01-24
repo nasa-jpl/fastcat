1dcdbee4fede35ff437208589d783b2999e350d7
Alex Brinkman <alexander.brinkman@jpl.nasa.gov>
2023-01-24 10:37:24 -0800

    add UTs for preventing taring in faulted state

--- test/test_unit/test_ati_fts.cc
+++ test/test_unit/test_ati_fts.cc
@@ -41,4 +41,20 @@ TEST_F(AtiFtsTest, ParseNominalConfig) {
   EXPECT_TRUE(device_.ConfigFromYaml(YAML::LoadFile(base_dir_+"nominal.yaml")));
 }
 
+// Currently the nominal behavior to faults is to prevent taring when faulted
+// This may change with optional YAML parameters in the future perhaps.
+TEST_F(AtiFtsTest, RejectTareWhenFaulted) {
+  EXPECT_TRUE(device_.ConfigFromYaml(YAML::LoadFile(base_dir_+"nominal.yaml")));
+
+  fastcat::DeviceCmd cmd;
+  cmd.type = fastcat::FTS_TARE_CMD;
+  EXPECT_TRUE(device_.Write(cmd));
+  MSG("Accepted tare when in nominal state");
+
+  device_.Fault();
+
+  cmd.type = fastcat::FTS_TARE_CMD;
+  EXPECT_FALSE(device_.Write(cmd));
+}
+
 } // namespace
