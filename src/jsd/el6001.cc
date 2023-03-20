8ad125d619e31fc1d5368e1446d16d870c8c84c3
David Kim <david.inkyu.kim@jpl.nasa.gov>
2023-03-20 14:01:37 -0700

    Added write for el6001, and offline codes

--- src/jsd/el6001.cc
+++ src/jsd/el6001.cc
@@ -32,6 +32,8 @@ bool fastcat::El6001::ConfigFromYamlCommon(YAML::Node node)
   }
   state_->name = name_;
 
+  jsd_slave_config_.el6001.baud_rate = JSD_EL6001_BAUD_RATE_115200;
+
   jsd_slave_config_.configuration_active = true;
   jsd_slave_config_.product_code         = JSD_EL6001_PRODUCT_CODE;
   snprintf(jsd_slave_config_.name, JSD_NAME_LEN, "%s", name_.c_str());
@@ -48,6 +50,36 @@ bool fastcat::El6001::Read()
 
   state_->el6001_state.statusword   = jsd_state->statusword;
   state_->el6001_state.controlword   = jsd_state->controlword_user;  
+
+  return true;
+}
+
+bool fastcat::El6001::Write(DeviceCmd& cmd)
+{
+
+  // If device supports async SDO requests
+  AsyncSdoRetVal sdoResult = WriteAsyncSdoRequest(cmd);
+  if(sdoResult != SDO_RET_VAL_NOT_APPLICABLE){
+    return (sdoResult == SDO_RET_VAL_SUCCESS);
+  }
+
+  if (cmd.type == EL6001_WRITE_DATA_CMD) {
+    uint8_t data_size = cmd.el6001_write_data_cmd.data_size;
+    if (data_size > JSD_EL6001_NUM_DATA_BYTES) {
+      ERROR("Data size must be in range (0,%u)", JSD_EL6001_NUM_DATA_BYTES);
+      return false;
+    }
+
+    for(int i =0; i < data_size; i++){
+      jsd_el6001_set_transmit_data_8bits(context_, slave_id_, i, cmd.el6001_write_data_cmd.data_out_bytes[i]);
+    }    
+
+    jsd_el6001_request_transmit_data(context_, slave_id_, data_size);
+  }
+  else {
+    ERROR("Bad EL6001 command");
+    return false;
+  }
 
   return true;
 }
---
+++ src/jsd/el6001_offline.h
@@ -0,0 +1,24 @@
+#ifndef FASTCAT_EL4102_OFFLINE_H_
+#define FASTCAT_EL4102_OFFLINE_H_
+
+// Include related header (for cc files)
+
+// Include c then c++ libraries
+
+// Include external then project includes
+#include "fastcat/jsd/el4102.h"
+
+namespace fastcat
+{
+class El4102Offline : public El4102
+{
+ public:
+  bool      ConfigFromYaml(YAML::Node node) override;
+  bool      Read() override;
+  FaultType Process() override;
+  bool      Write(DeviceCmd& cmd) override;
+};
+
+}  // namespace fastcat
+
+#endif
