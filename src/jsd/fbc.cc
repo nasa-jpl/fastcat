0b89040248dce8e6dcf66ef59b5acdd2069eef20
David Kim <david.inkyu.kim@jpl.nasa.gov>
2023-04-04 11:47:06 -0700

    Adding Read function

--- src/jsd/fbc.cc
+++ src/jsd/fbc.cc
@@ -11,6 +11,12 @@
 // Include external then project includes
 #include "fastcat/yaml_parser.h"
 
+#define HEX_FORMAT "0x%02X"
+
+#define FBC_START_BYTE 0x53  ///< ACSII 'S'
+#define FBC_STOP_BYTE  0x45  ///< ACSII 'E'
+#define FBC_DATA_PACKET_SIZE 13
+
 fastcat::Fbc::Fbc()
 {
   MSG_DEBUG("Constructed Fbc");
@@ -47,18 +53,29 @@ bool fastcat::Fbc::ConfigFromYamlCommon(YAML::Node node)
 
 bool fastcat::Fbc::Read()
 {
+  bool ready_to_parse = false;
+
   jsd_el6001_read((jsd_t*)context_, slave_id_);
 
   const jsd_el6001_state_t* jsd_state =
       jsd_el6001_get_state((jsd_t*)context_, slave_id_);
 
-  state_->el6001_state.statusword   = jsd_state->statusword;
-  state_->el6001_state.controlword   = jsd_state->controlword_user;
+  // Each persistent received bytes is actually a uint8_t, but the local
+  // variable here uses uint32_t so that bit shifting and masking operations
+  // are written out intuitively.
+  uint32_t received_bytes[FBC_DATA_PACKET_SIZE] = {0};
 
-  for(int i=0; i < JSD_EL6001_NUM_DATA_BYTES; i++){
-    state_->el6001_state.data_in_bytes[i] = jsd_state->received_bytes[i];
-    state_->el6001_state.data_out_bytes[i] = jsd_state->transmit_bytes[i];
+  if (jsd_state->received_all_persistent_bytes) // assume EL6001 state is in READY_TO_COMMUNICATE
+  {
+    for(int i=0; i < FBC_DATA_PACKET_SIZE; i++){
+      received_bytes[i] = jsd_state->persistent_received_bytes[i];
+    }
+    ready_to_parse = true;
   }
+
+  if (ready_to_parse){
+    // parse received bytes into FBC State
+  }  
 
   return true;
 }
