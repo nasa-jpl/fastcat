7b10dd2d708b4190c30c6d4bd6862cb1ae88ff27
Daniel Loret de Mola Lemus <daniel.loret@jpl.nasa.gov>
2023-06-07 19:38:13 -0700

    Updated product code in Platinum driver and fixed build error with test_trap.cc

--- src/jsd/platinum_actuator.cc
+++ src/jsd/platinum_actuator.cc
@@ -11,7 +11,7 @@ fastcat::PlatinumActuator::PlatinumActuator() { state_->type = PLATINUM_ACTUATOR
 
 void fastcat::PlatinumActuator::PopulateJsdSlaveConfig()
 {
-  jsd_slave_config_.product_code = JSD_EPD_PRODUCT_CODE;
+  jsd_slave_config_.product_code = JSD_EPD_PRODUCT_CODE_STD_FW;
 
   jsd_slave_config_.epd.max_motor_speed   = EuToCnts(params_.max_speed_eu_per_sec);
   jsd_slave_config_.epd.loop_period_ms    = lround(loop_period_ * 1000.0);
