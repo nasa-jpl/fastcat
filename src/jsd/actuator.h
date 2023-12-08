392939ce4b6fac85387b73b6e96482eb6b295a48
Kristopher Wehage <kwehage@jpl.nasa.gov>
2023-12-08 13:05:40 -0800

    Increase buffer size for command messages

--- src/jsd/actuator.h
+++ src/jsd/actuator.h
@@ -262,9 +262,9 @@ class Actuator : public JsdDeviceBase
 
   bool    actuator_absolute_encoder_ = false;
   int32_t elmo_pos_offset_cnts_      = 1;
-  RingBuffer<DeviceCmd> last_device_cmd_ = RingBuffer<DeviceCmd>(15);
+  RingBuffer<DeviceCmd> last_device_cmd_ = RingBuffer<DeviceCmd>(50);
   double csp_interpolation_offset_time_ = 0.0;
-  size_t csp_cycles_delay_ = 4; 
+  size_t csp_cycles_delay_ = 4;
   size_t csp_cycles_stale_ = 10;
 
   ActuatorExplicitInterpolationAlgorithm explicit_interpolation_algorithm_ = 
