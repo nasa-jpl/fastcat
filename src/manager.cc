a881e469373c292bb75c9baf4f372e627441c284
prestonr <prestonr@jpl.nasa.gov>
2024-07-25 18:06:08 -0700

    fix: not using the last_wkc condition

--- src/manager.cc
+++ src/manager.cc
@@ -228,9 +228,6 @@ bool fastcat::Manager::Process(double external_time)
     auto jsd = it->second;
     jsd_read(jsd, 1e6 / target_loop_rate_hz_);
     if (jsd->wkc != jsd->expected_wkc) {
-      ERROR("Work counter: %d, Expected work counter: %d, last work counter: %d", jsd->wkc, jsd->expected_wkc, jsd->last_wkc);
-    }
-    if (jsd->wkc != jsd->expected_wkc && jsd->last_wkc != jsd->wkc) {
       ERROR("Bad working counter experienced on jsd bus %s", ifname.c_str());
       ExecuteAllDeviceFaults();
     }
