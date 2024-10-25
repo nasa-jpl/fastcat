8a606bc5fd91fde12fa3e94c69a874e55bb0e86c
prestonr <prestonr@jpl.nasa.gov>
2024-10-24 17:30:30 -0700

    Modified fastcat to include every setup parameter

--- doc/fastcat_device_config_parameters.md
+++ doc/fastcat_device_config_parameters.md
@@ -15,6 +15,7 @@ For every `JSD Device` there is an `Offline Device` to emulate the behavior of t
 | El3602           | Beckhoff      | 2-channel +/-10v Diff. Analog Input       |
 | El2124           | Beckhoff      | 4-channel 5v Digital Output               |
 | El2809           | Beckhoff      | 16-channel 24v Digital Output             |
+| El5042           | Beckhoff      | 2-channel Encoder             |
 | El4102           | Beckhoff      | 2-channel 0-10v Analog Output             |
 | Ild1900          | Micro-Epsilon | Distance Laser Sensor                     |
 | AtiFts           | ATI           | Force-Torque Sensor                       |
@@ -423,6 +424,51 @@ The permitted range values are:
 ``` yaml
 - device_class: El2809
   name: el2809_1
+```
+
+## El5042 (2-channel BiSS-C Encoder Interface)
+
+| Parameter         | Description                                                  |
+| ----------------- | ------------------------------------------------------------ |
+| `invert_feedback_direction`         | Negates the position value (0 for don't invert and 1 for invert) |
+| `disable_status_bits`      | Tell the slave whether or not to send status bits (0 for don't disable and 1 for disable) |
+| `invert_checksum` | Inverts the checksum bits (CRC) received by the encoder (0 for don't inver and 1 for invert) |
+| `checksum_polynomial`   | Polynomial used for calculating checksum. This is a 32 bit polynomial. |
+| `supply_voltage`  | Set the encoder supply voltage, either 50 for 5V or 90 for 9V |
+| `clock_frequency`  | Clock frequency for the BiSS-C protocol (250 kHz to 10 MHz as specified below) |
+| `gray_code`  | Option to either use dual code or gray code for accurate data (0 for dual code and 1 for gray code) |
+| `multiturn_bits`  | Set the number of multiturn bits (how many complete rotations) |
+| `singleturn_bits`  | Set the number of singleturn bits (resolution of a single rotation) |
+| `offset_bits`  | If there are addition null bits at the end of the packet, we shift by offset bits to get data |
+| `ssi_mode`  | Opt for SSI mode over BiSS-C (0 for BiSS-C and 1 for SSI) |
+
+Clock frequency correspondances
+
+* 0: 10 MHz
+* 1: 5 MHz
+* 2: 3.3 MHz
+* 3: 2.5 MHz
+* 4: 2 MHz
+* 9: 1 MHz
+* 17: 500 kHz
+* 19: 250 kHz
+
+#### Example
+
+```yaml
+- device_class: El5042
+  name: el5042_1
+  invert_feedback_direction: [0,  0]
+  disable_status_bits:       [0,  0]
+  invert_checksum:           [1,  1]
+  checksum_polynomial:       [67, 67]
+  supply_voltage:            [50, 50]
+  clock_frequency:           [1,  1]
+  gray_code:                 [0,  0]
+  multiturn_bits:            [0,  0]
+  singleturn_bits:           [19, 19]
+  offset_bits:               [0,  0]
+  ssi_mode:                  [0,  0]
 ```
 
 ## El4102 (2-channel 0-10v Analog Output)
--- src/jsd/el5042.cc
+++ src/jsd/el5042.cc
@@ -32,6 +32,112 @@ bool fastcat::El5042::ConfigFromYamlCommon(const YAML::Node& node)
   jsd_slave_config_.driver_type          = JSD_DRIVER_TYPE_EL5042;
   snprintf(jsd_slave_config_.name, JSD_NAME_LEN, "%s", name_.c_str());
 
+  YAML::Node invert_feedback_direction_node;
+  if (!ParseList(node, "invert_feedback_direction", invert_feedback_direction_node)) {
+    return false;
+  }
+
+  YAML::Node disable_status_bits_node;
+  if (!ParseList(node, "disable_status_bits", disable_status_bits_node)) {
+    return false;
+  }
+
+  YAML::Node invert_checksum_node;
+  if (!ParseList(node, "invert_checksum", invert_checksum_node)) {
+    return false;
+  }
+
+  YAML::Node checksum_polynomial_node;
+  if (!ParseList(node, "checksum_polynomial", checksum_polynomial_node)) {
+    return false;
+  }
+
+  YAML::Node supply_voltage_node;
+  if (!ParseList(node, "supply_voltage", supply_voltage_node)) {
+    return false;
+  }
+
+  YAML::Node clock_frequency_node;
+  if (!ParseList(node, "clock_frequency", clock_frequency_node)) {
+    return false;
+  }
+
+  YAML::Node gray_code_node;
+  if (!ParseList(node, "gray_code", gray_code_node)) {
+    return false;
+  }
+
+  YAML::Node multiturn_bits_node;
+  if (!ParseList(node, "multiturn_bits", multiturn_bits_node)) {
+    return false;
+  }
+
+  YAML::Node singleturn_bits_node;
+  if (!ParseList(node, "singleturn_bits", singleturn_bits_node)) {
+    return false;
+  }
+
+  YAML::Node offset_bits_node;
+  if (!ParseList(node, "offset_bits", offset_bits_node)) {
+    return false;
+  }
+
+  YAML::Node ssi_mode_node;
+  if (!ParseList(node, "ssi_mode_bits", ssi_mode_node)) {
+    return false;
+  }
+
+  // Check the right number of entries in each array
+  if (invert_feedback_direction_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      disable_status_bits_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      invert_checksum_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      checksum_polynomial_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      supply_voltage_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      clock_frequency_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      gray_code_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      multiturn_bits_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      singleturn_bits_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      offset_bits_node.size() != JSD_EL5042_NUM_CHANNELS ||
+      ssi_mode_node.size() != JSD_EL5042_NUM_CHANNELS) {
+    return false;
+  }
+
+  int ii;
+  for (ii = 0; ii < JSD_EL3208_NUM_CHANNELS; ii++) {
+    jsd_slave_config_.el5042.invert_feedback_direction[ii] =
+        invert_feedback_direction_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.disable_status_bits[ii] =
+        disable_status_bits_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.invert_checksum[ii] =
+        invert_checksum_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.checksum_polynomial[ii] =
+        checksum_polynomial_node[ii].as<uint32_t>();
+
+    jsd_slave_config_.el5042.supply_voltage[ii] =
+        supply_voltage_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.clock_frequency[ii] =
+        clock_frequency_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.gray_code[ii] =
+        gray_code_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.multiturn_bits[ii] =
+        multiturn_bits_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.singleturn_bits[ii] =
+        singleturn_bits_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.offset_bits[ii] =
+        offset_bits_node[ii].as<uint8_t>();
+
+    jsd_slave_config_.el5042.ssi_mode[ii] =
+        ssi_mode_node[ii].as<uint8_t>();
+  }
+
   return true;
 }
 
